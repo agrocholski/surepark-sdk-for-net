f2529b38a34f71ac406efeeca9a9b19c87ee095b
adam grocholski <adam@thinkfirstcodelater.com>
2014-04-15 19:33:03 -0500

    added SureParkClient class
    props and ctor

---
+++ sdk/SurePark.Core/SurePark.Core/SureParkClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurePark.Core
+{
+    public class SureParkClient
+    {
+        public SureParkClient()
+            : this("http://www.mspairport.com/data/sureparknow/surepark.aspx",
+            "http://www.mspairport.com/data/sureparknow/info.xml") { }
+
+        public SureParkClient(string availabilityUrl, string rateUrl)
+        {
+            if (string.IsNullOrEmpty(availabilityUrl))
+                throw new ArgumentNullException("availabilityUrl");
+
+            if (string.IsNullOrEmpty(rateUrl))
+                throw new ArgumentNullException("rateUrl");
+
+            AvailabilityUrl = availabilityUrl;
+            RateUrl = availabilityUrl;
+        }
+
+        public string AvailabilityUrl { get; set; }
+        public string RateUrl { get; set; }
+    }
+}
