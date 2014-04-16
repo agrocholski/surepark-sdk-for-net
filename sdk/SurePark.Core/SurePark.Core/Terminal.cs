d64c8e909d28fb00e0de85edd3118a630d76f077
adam grocholski <adam@thinkfirstcodelater.com>
2014-04-15 19:23:46 -0500

    added Terminal class

---
+++ sdk/SurePark.Core/SurePark.Core/Terminal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurePark.Core
+{
+    public class Terminal
+    {
+        public Terminal()
+        {
+            ParkingRates = new List<ParkingRate>();
+        }
+
+        public string Name { get; set; }
+        public string ParkingAvailability { get; set; }
+        public List<ParkingRate> ParkingRates { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
